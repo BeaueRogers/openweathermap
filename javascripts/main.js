dcd90f52607a400a97a37139cc2e240700476697
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-11-07 12:51:13 -0600

    templates.js added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,12 @@
 requirejs.config({
     baseUrl: './javascripts',
     paths: {
-        'q': '../lib/bower_components/q/q',
         'jquery': '../lib/bower_components/jquery/dist/jquery.min',
         'lodash': '../lib/bower_components/lodash/lodash.min',
         'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
         'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
         'firebase': '../lib/bower_components/firebase/firebase'
+        'q': '../lib/bower_components/q/q',
     },
     shim: {
         'bootstrap': ['jquery']
@@ -14,8 +14,8 @@ requirejs.config({
 });
 
 requirejs(
-    ["jquery", "hbs", "bootstrap", "templates", "userAuth"],
-    function($, Handlebars, bootstrap, templates, userAuth) {
+    ["jquery", "hbs", "bootstrap", "templates", "userAuth", "q"],
+    function($, Handlebars, bootstrap, templates, userAuth, q) {
 
 
 });
---
+++ javascripts/templates.js
@@ -0,0 +1,11 @@
+define(["hbs",
+        "hbs!../templates/login"],
+
+function(handlebars, login) {
+
+  var templates = {};
+  templates.login = login;
+
+  return templates;
+
+});
